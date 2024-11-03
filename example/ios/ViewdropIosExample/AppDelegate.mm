c2899002c01e39421f10b5d75651a2f93058ffa2
Чащухин Вячеслав Вадимович <chaschyukhin_v@dogma.ru>
2024-11-03 20:24:13 +0300

    fix: fix build troubles

--- example/ios/ViewdropIosExample/AppDelegate.mm
+++ example/ios/ViewdropIosExample/AppDelegate.mm
@@ -23,4 +23,13 @@
 #endif
 }
 
+- (NSURL *)bundleURL
+{
+#if DEBUG
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+#else
+  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
+}
+
 @end
