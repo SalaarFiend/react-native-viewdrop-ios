9f6acaef41d4687c4481cd988d4d179ec625e95d
SalaarFiend <slava.temna@gmail.com>
2025-03-11 00:47:34 +0300

    feat(lib): delete comments

--- ios/ViewDropModuleBridge.m
+++ ios/ViewDropModuleBridge.m
@@ -1,11 +1,3 @@
-//
-//  ViewDropModule.m
-//  SquadusRN
-//
-//  Created by user on 19.02.2023.
-//  Copyright © 2023 Facebook. All rights reserved.
-//
-
 #import <React/RCTBridgeModule.h>
 
 @interface RCT_EXTERN_MODULE(ViewDropModule, UIView)
